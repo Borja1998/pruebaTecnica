fac14f36c39e64bed0f61bf3e0ddb1a72aa67ffb
Imran Imtiaz <imran.imtiaz@daralber.ae>
2024-07-02 07:19:25 +0400

    comments remove from script

--- DB_prueba_tecnica.sql
+++ DB_prueba_tecnica.sql
@@ -1,23 +1,22 @@
--- Drop the database if it exists and create a new one
+
 DROP DATABASE IF EXISTS db_prueba_tecnica;
 CREATE DATABASE db_prueba_tecnica;
 
--- Use the newly created database
+
 USE db_prueba_tecnica;
 
--- Create the Articles table
+
 CREATE TABLE Articles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titular VARCHAR(255) NOT NULL,
     contenido TEXT NOT NULL
 );
 
--- Insert records into the Articles table
+
 INSERT INTO Articles (titular, contenido) VALUES ('Nombre', 'este es el nombre del usuario');
 INSERT INTO Articles (titular, contenido) VALUES ('apellido1', 'Primer apellido del usuario');
 INSERT INTO Articles (titular, contenido) VALUES ('apellido2', 'segundo apellido del usuario');
 INSERT INTO Articles (titular, contenido) VALUES ('edad', 'estos son los años del usuario');
 INSERT INTO Articles (titular, contenido) VALUES ('mes', 'aquí se pone el mes de nacimiento');
 
--- Select all records from the Articles table
 SELECT * FROM Articles;
